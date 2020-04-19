160886b01b2823d0f2b4759b4ccd2afd704d8ecf
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-04-19 06:34:58 +0200

    algos.clj: Normalize input value
    The csv can contain 0/1 instead of false/true.

--- src/algos.clj
+++ src/algos.clj
@@ -1,41 +1,11 @@
 (ns algos
   (:require [clojure.edn :as edn]))
 
-(def s edn/read-string)
-
-;; (def data {:heart_disease                  "0",
-;;            :postal_code                    "37",
-;;            :temperature_cat                "NSP",
-;;            :date                           "2020-04-15T21:59:46.656461Z",
-;;            :cough                          "true",
-;;            :immunosuppressant_disease_algo "false",
-;;            :heart_disease_algo             "false",
-;;            :liver_disease                  "false",
-;;            :age_range                      "from_15_to_49",
-;;            :form_version                   "2020-04-06",
-;;            :pregnant                       "888",
-;;            :immunosuppressant_disease      "0",
-;;            :orientation                    "orientation_consultation_surveillance_3",
-;;            :kidney_disease                 "false",
-;;            :duration                       "230",
-;;            :sore_throat_aches              "false",
-;;            :immunosuppressant_drug_algo    "false",
-;;            :breathing_disease              "false",
-;;            :line                           1,
-;;            :diabetes                       "false",
-;;            :algo_version                   "2020-04-06",
-;;            :id                             "28ef0394-741f-4f4c-8c14-632843a24a9c",
-;;            :imc                            "24.1",
-;;            :fever                          "999",
-;;            :cancer                         "false",
-;;            :fever_algo                     "false",
-;;            :agueusia_anosmia               "false",
-;;            :tiredness                      "false",
-;;            :breathlessness                 "false",
-;;            :immunosuppressant_drug         "0",
-;;            :diarrhea                       "true",
-;;            :tiredness_details              "false",
-;;            :feeding_day                    "false"})
+(defn normalize [^String s]
+  (let [val-s (edn/read-string s)]
+    (condp #(= %1 (type %2)) val-s
+      java.lang.Boolean val-s
+      java.lang.Long    (condp = val-s 1 true 0 false val-s))))
 
 (defn preprocess-2020-04-06
   [{:keys [imc age_range
@@ -53,23 +23,24 @@
         pronostic-factors      (atom 0)]
     ;; Compute pronostic factors
     (when (= age_range "sup_70") (swap! pronostic-factors inc))
-    (when (>= (s imc) 30) (swap! pronostic-factors inc))
-    (when (s heart_disease_algo) (swap! pronostic-factors inc))
-    (when (s immunosuppressant_disease_algo) (swap! pronostic-factors inc))
-    (when (s immunosuppressant_drug_algo) (swap! pronostic-factors inc))
-    (when (s kidney_disease) (swap! pronostic-factors inc))
-    (when (s liver_disease) (swap! pronostic-factors inc))
-    (when (s breathing_disease) (swap! pronostic-factors inc))
-    (when (s cancer) (swap! pronostic-factors inc))
-    (when (s diabetes) (swap! pronostic-factors inc))
-    (when (= (s pregnant) 1) (swap! pronostic-factors inc))
+    (when (>= (normalize imc) 30) (swap! pronostic-factors inc))
+    (when (normalize heart_disease_algo) (swap! pronostic-factors inc))
+    (when (normalize immunosuppressant_disease_algo) (swap! pronostic-factors inc))
+    (when (normalize immunosuppressant_drug_algo) (swap! pronostic-factors inc))
+    (when (normalize kidney_disease) (swap! pronostic-factors inc))
+    (when (normalize liver_disease) (swap! pronostic-factors inc))
+    (when (normalize breathing_disease) (swap! pronostic-factors inc))
+    (when (normalize cancer) (swap! pronostic-factors inc))
+    (when (normalize diabetes) (swap! pronostic-factors inc))
+    (when (and (normalize pregnant) (not (= (normalize pregnant) 999)))
+      (swap! pronostic-factors inc))
     ;; Compute minor-severity-factors
-    (when (s fever_algo) (swap! minor-severity-factors inc))
-    (when (and (s tiredness) (s tiredness_details))
+    (when (normalize fever_algo) (swap! minor-severity-factors inc))
+    (when (and (normalize tiredness) (normalize tiredness_details))
       (swap! minor-severity-factors inc))
     ;; Compute major-severity-factors
-    (when (s breathlessness) (swap! major-severity-factors inc))
-    (when (s feeding_day) (swap! major-severity-factors inc))
+    (when (normalize breathlessness) (swap! major-severity-factors inc))
+    (when (normalize feeding_day) (swap! major-severity-factors inc))
     {:major-severity-factors @major-severity-factors
      :minor-severity-factors @minor-severity-factors
      :pronostic-factors      @pronostic-factors}))
@@ -82,11 +53,11 @@
            pronostic-factors
            major-severity-factors
            minor-severity-factors]}]
-  (let [fever_algo        (s fever_algo)
-        diarrhea          (s diarrhea)
-        cough             (s cough)
-        sore_throat_aches (s sore_throat_aches)
-        agueusia_anosmia  (s agueusia_anosmia)]
+  (let [fever_algo        (normalize fever_algo)
+        diarrhea          (normalize diarrhea)
+        cough             (normalize cough)
+        sore_throat_aches (normalize sore_throat_aches)
+        agueusia_anosmia  (normalize agueusia_anosmia)]
     (cond
       ;; Branche 1
       (= age_range "inf_15")
@@ -135,11 +106,11 @@
            pronostic-factors
            major-severity-factors
            minor-severity-factors]}]
-  (let [fever_algo        (s fever_algo)
-        diarrhea          (s diarrhea)
-        cough             (s cough)
-        sore_throat_aches (s sore_throat_aches)
-        agueusia_anosmia  (s agueusia_anosmia)]
+  (let [fever_algo        (normalize fever_algo)
+        diarrhea          (normalize diarrhea)
+        cough             (normalize cough)
+        sore_throat_aches (normalize sore_throat_aches)
+        agueusia_anosmia  (normalize agueusia_anosmia)]
     (cond
       ;; Branche 1
       (= age_range "inf_15")
